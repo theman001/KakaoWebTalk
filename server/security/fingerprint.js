36a55928f1f7cfac6650d236d260d1ccabe9cb2f
theman001 <100943215+theman001@users.noreply.github.com>
2026-01-04 20:46:10 +0900

    Update and rename fingerprint.js to abuse.js

--- server/security/fingerprint.js
+++
@@ -1,8 +0,0 @@
-// security/fingerprint.js
-import crypto from "crypto";
-
-export function buildDeviceFingerprint(deviceInfo) {
-     const seed = `${deviceInfo.model}|${deviceInfo.os}|${deviceInfo.ram}`;
-     const fp = crypto.createHash("sha256").update(seed).digest("hex");
-     return fp;
-}
